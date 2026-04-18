4c36303e19e3a4ae349fafaf5aeeed3e17e44397
Tosin <adeolamimilabake@gmail.com>
2026-04-18 18:35:44 +0100

    Countdown from user input

---
+++ touchGrass/CountDown.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CountDown{
+    public static void main(String[] args){
+        Scanner numberCollector = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int userInput = numberCollector.nextInt();
+        for(int number = userInput; number >= 1; number--){
+            System.out.println(number);
+            
+        }System.out.println("Blast off!");
+    }
+}
