3c9b8c81d2dc7ca5ab3ed61cf5859669e3ef35b4
Tosin <adeolamimilabake@gmail.com>
2026-04-18 17:49:59 +0100

    five times multiplication table

---
+++ touchGrass/MultiplicationTableOfFive.java
@@ -0,0 +1,8 @@
+public class MultiplicationTableOfFive{
+    public static void main(String[] args){
+        for(int number = 1; number <= 12; number++){
+            int product = 5 * number;
+            System.out.println("5 x " + number + " = " + product);
+        }
+}
+}
