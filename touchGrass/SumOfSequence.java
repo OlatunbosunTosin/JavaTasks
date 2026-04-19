3224607394659d85cc5027f1260204c32e91fd41
Tosin <adeolamimilabake@gmail.com>
2026-04-19 15:40:16 +0100

    Sum of numbers in a sequence

--- touchGrass/SumOfSequence.java
+++ touchGrass/SumOfSequence.java
@@ -2,14 +2,14 @@ public class SumOfSequence{
     public static void main(String[] args){
         int numerator = -1;
         int denominator = 1;
-        double sum_of_sequence = 0.0;
+        double sumOfSequence = 0.0;
         for(;numerator < 99;){
             numerator+=2;
             denominator+=2;
             
             double fraction = (double) numerator / denominator;
-            sum_of_sequence += fraction;
+            sumOfSequence += fraction;
      
-        }System.out.printf("sum of sequence is %.4f", sum_of_sequence);
+        }System.out.printf("sum of sequence is %.4f", sumOfSequence);
     }
 }
