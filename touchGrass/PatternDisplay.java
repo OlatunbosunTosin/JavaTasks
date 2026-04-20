c06a224e570a51008fb4877724f122b98fea5d8a
Tosin <adeolamimilabake@gmail.com>
2026-04-20 14:53:09 +0100

    Display of 7 to 7

---
+++ touchGrass/PatternDisplay.java
@@ -0,0 +1,11 @@
+public class PatternDisplay{
+    public static void main(String[] args){
+        for(int number = 7; number >= 1; number--){
+            System.out.print(number);
+        }
+        for(int integer = 2; integer <= 7; integer++){
+            System.out.print(integer);   
+        }
+        
+    }
+}
