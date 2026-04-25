13016e37e940ca3e350571ebda5ab795cb386c56
Tosin <adeolamimilabake@gmail.com>
2026-04-25 14:39:25 +0100

    prime numbers

--- touchGrass/PrimeNumbers.java
+++ touchGrass/PrimeNumbers.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 public class PrimeNumbers{
     public static void main(String[] args){
-        for(int number = 2; number <= 1200; number++){
-            for(int numbers = 2; numbers <= 1200; numbers++){
-                if(number % numbers == 0 &&  number % 1 == number && number % numbers != 0){
-                System.out.print(number);
-                }
-            }
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int count = 0;
+        int factor = 1; 
+        for (;factor <= number; factor++){
+            if (number % factor == 0){
+                count++;
+            } 
         }
+        if (count == 2){
+            System.out.printf("%d is a prime number%n", number);
+        }
+        else{
+            System.out.printf("%d is not a prime number%n", number);
+        }
+                
     }
 }
