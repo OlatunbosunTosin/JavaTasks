982cfd1c87903f862aa90b4a36e4923e5edbc3fe
Tosin <adeolamimilabake@gmail.com>
2026-05-13 02:11:31 +0100

    Discount Enginr for Promo

---
+++ wahala/OshoFreePromoEngine.java
@@ -0,0 +1,49 @@
+public class OshoFreePromoEngine{
+
+    public static double getDiscountOf(double cartTotal, String promoCode){
+    
+        double discount = 0.0;
+        double finalPrice = 0.0;
+        
+        if (cartTotal <= 0 ){
+        
+            throw new IllegalArgumentException("Amount cannot be negative");
+            
+        }
+        
+        else if (cartTotal > 0.0 && cartTotal < 5000.00){
+        
+            finalPrice = cartTotal;
+            
+        }
+        
+        else if (cartTotal >= 5000.00 && cartTotal <= 14999.00 && promoCode.equals("STARTER10")){
+        
+            discount = (10.0 / 100.0) * cartTotal;
+            finalPrice = cartTotal - discount;
+        
+        }
+        
+        else if (cartTotal > 14999.00 && cartTotal <= 29999 && promoCode.equals("BIGBOY20")){
+        
+            discount = (20.0 / 100.0) * cartTotal;
+            finalPrice = cartTotal - discount;
+        
+        }
+        
+        else if (cartTotal > 29999.00 && promoCode.equals("OSHOFREE35")){
+        
+            discount = (35.0 / 100.0) * cartTotal;
+            finalPrice = cartTotal - discount;
+        
+        }
+        else
+            finalPrice = cartTotal;
+            
+        return finalPrice;
+    
+    
+    }
+
+
+}
---
+++ wahala/OshoFreePromoEngineTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OshoFreePromoEngineTest{
+
+    @Test
+    public void testForDiscountOfZeroPercent(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(4000.00, "STARTER10");
+        double actualDiscountedPrice = 4000.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+    
+    @Test
+    public void testForDiscountOfTenPercent(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(6000.00, "STARTER10");
+        double actualDiscountedPrice = 5400.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+    
+    @Test
+    public void testForDiscountOfTwentyPercent(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(18000.00, "BIGBOY20");
+        double actualDiscountedPrice = 14400.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+    
+    @Test
+    public void testForDiscountOfThirtyFivePercent(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(40000.00, "OSHOFREE35");
+        double actualDiscountedPrice = 26000.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+        @Test
+    public void testForInvalidPromoCode(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(18000.00, "FAKE99");
+        double actualDiscountedPrice = 18000.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+        
+        double expectedDiscountedPrice2 = OshoFreePromoEngine.getDiscountOf(50000.00, "STARTER10");
+        double actualDiscountedPrice2 = 50000.00;
+        assertEquals(expectedDiscountedPrice2, actualDiscountedPrice2);
+    
+    }
+    
+        @Test
+    public void testForCartTotalOfFiveThousand(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(5000.00, "STARTER10");
+        double actualDiscountedPrice = 4500.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+        @Test
+    public void testForCartTotalOfFifteenThousand(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(15000.00, "BIGBOY20");
+        double actualDiscountedPrice = 12000.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+        @Test
+    public void testForCartTotalOfThirtyThousand(){
+    
+        double expectedDiscountedPrice = OshoFreePromoEngine.getDiscountOf(30000.00, "OSHOFREE35");
+        double actualDiscountedPrice = 19500.00;
+        assertEquals(expectedDiscountedPrice, actualDiscountedPrice);
+    
+    }
+    
+        @Test
+    public void testForInvalidAmount(){
+
+        assertThrows(IllegalArgumentException.class, () -> OshoFreePromoEngine.getDiscountOf(-2000.00, "OSHOFREE35"));
+    
+    }
+
+}
